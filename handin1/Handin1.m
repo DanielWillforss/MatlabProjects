37f92395d607715e5b11aedd72fc909accd24f33
callmederp <willforss66@gmail.com>
2026-04-15 09:26:23 +0200

    worked on handin1

--- handin1/Handin1.m
+++ handin1/Handin1.m
@@ -16,8 +16,8 @@ function out = findLargest(v, n)
 end
 
 function W_comp = largestComponent(W)
-    G = graph(W);
-    bins = conncomp(G);
+    G = digraph(W);
+    bins = conncomp(G, 'Type','strong');
 
     counts = histcounts(bins, 1:(max(bins)+1));
     [~, largestID] = max(counts);
@@ -35,20 +35,146 @@ function z = eigenvectorCentrality(W)
     z = z / norm(z);
 end
 
+function z = katzCentrality(W, beta, mu)
+    lambda = eigs(W', 1);
+
+
+    z = (eye(size(W)) - (1-beta)*W'/lambda)\(beta*mu); %\ is inverse
+end
+
+function z = iterativePageRank(W, beta, mu)
+    [w_out, ~] = degree(W);
+    dangling = (w_out == 0);
+
+    w_out(dangling) = 1;
+    
+    D = sparse(diag(w_out));
+    P = D \ W;
+    
+    % fix dangling row properly (PageRank teleportation idea)
+    P(dangling,:) = 1/length(w_out);
+
+
+    mu = mu / sum(mu);
+    z = mu; %initial value
+
+    for k = 1:1000
+        z_new = (1 - beta) * P' * z + beta * mu;
+        
+        if norm(z_new - z) < 1e-8
+            k
+            break;
+        end
+        
+        z = z_new;
+    end
+    
+    z = z_new;
+end
+
+%%
+
+beta = 0.15;
+mu = ones(length(W), 1);
+
+pageRank = iterativePageRank(W, beta, mu);
+top_pageRank = findLargest(pageRank, 5);
+
 %% Centrality
 
-A = randi(100, 500, 500);   % 500x500 matrix with integers 1–100
+% general
+% swe2000, idn2000
+% find 3 most central sectors
 
-[w_out, w_in] = degree(A);
+load("IOdownload.mat");
+% econ: countries and years
+% io: data
+% name: sectors
 
-out = findLargest(w_out, 10);
-in = findLargest(w_in, 10);
+% W
+swe2000 = io.swe2000;
+idn2000 = io.idn2000;
 
-z = eigenvectorCentrality(largestComponent(A));
-center = findLargest(z, 10);
+%% a) in/out
 
+[swe_in, swe_out] = degree(swe2000);
+[idn_in, idn_out] = degree(idn2000);
+
+disp("Swe in")
+top_swe_in = findLargest(swe_in, 3);
+disp(name(top_swe_in(:,1)));
+disp(top_swe_in(:,2)');
+disp("Swe out")
+top_swe_out = findLargest(swe_out, 3);
+disp(name(top_swe_out(:,1)));
+disp(top_swe_out(:,2)');
+disp("Idn in")
+top_idn_in = findLargest(idn_in, 3);
+disp(name(top_idn_in(:,1)));
+disp(top_idn_in(:,2)');
+disp("Idn out")
+top_idn_out = findLargest(idn_out, 3);
+disp(name(top_idn_out(:,1)));
+disp(top_idn_out(:,2)');
+
+%% b) eigenvector
+swe_comp = largestComponent(swe2000);
+idn_comp = largestComponent(idn2000);
+swe_eigen = eigenvectorCentrality(swe_comp);
+idn_eigen = eigenvectorCentrality(idn_comp);
+top_swe_eigen = findLargest(swe_eigen, 3);
+top_idn_eigen = findLargest(idn_eigen, 3);
+disp("top swe eigen")
+disp(name(top_swe_eigen(:,1)));
+disp(top_swe_eigen(:,2)');
+disp("top idn eigen")
+disp(name(top_idn_eigen(:,1)));
+disp(top_idn_eigen(:,2)');
+
+%% c) Katz
+n = length(swe2000);
+beta = 0.15;
+mu1 = ones(n, 1);
+mu2 = zeros(n, 1);
+mu2(31) = 1;
+
+swe_katz_1 = katzCentrality(swe2000, beta, mu1);
+swe_katz_2 = katzCentrality(swe2000, beta, mu2);
+idn_katz_1 = katzCentrality(idn2000, beta, mu1);
+idn_katz_2 = katzCentrality(idn2000, beta, mu2);
+
+top_swe_katz_1 = findLargest(swe_katz_1, 3);
+top_swe_katz_2 = findLargest(swe_katz_2, 3);
+top_idn_katz_1 = findLargest(idn_katz_1, 3);
+top_idn_katz_2 = findLargest(idn_katz_2, 3);
+
+disp("top swe katz ones")
+disp(name(top_swe_katz_1(:,1)));
+disp(top_swe_katz_1(:,2)');
+disp("top swe katz single one")
+disp(name(top_swe_katz_2(:,1)));
+disp(top_swe_katz_2(:,2)');
+disp("top idn katz ones")
+disp(name(top_idn_katz_1(:,1)));
+disp(top_idn_katz_1(:,2)');
+disp("top idn katz single ones")
+disp(name(top_idn_katz_2(:,1)));
+disp(top_idn_katz_2(:,2)');
 %% Influence
 
+users = load('users.mat', '-ascii');
+twitter = load('twitter.mat', '-ascii');
+n = length(users);
+W = spconvert([twitter; n n 0]); %%Would overwrite a selfloop in final node
+
+%% a)
+beta = 0.15;
+mu = ones(length(W), 1);
+
+pageRank = iterativePageRank(W, 0.15, mu);
+top_pageRank = findLargest(pageRank, 5);
+
+%% a)
 
 
 
